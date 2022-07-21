85c08ff12d485e5112a418aab7092b6059b2f4d4
Maksims Svecovs <maksims.svecovs@arm.com>
2022-07-21 16:02:22 +0100

    ACS: fix CROSS_COMPILE variables
    Signed-off-by: Maksims Svecovs <maksims.svecovs@arm.com>
    Change-Id: Ib60cca02881eccfecb9278d9fa1d010d12def496

--- acs/builder.sh
+++ acs/builder.sh
@@ -11,13 +11,13 @@ mkdir build
 cd build
 echo "Building ACS test suite."
 cmake ../ -G"Unix Makefiles" \
-	-DCROSS_COMPILE=$CROSS_COMPILE \
+	-DCROSS_COMPILE=aarch64-none-elf- \
 	-DTARGET=tgt_tfa_fvp \
 	-DPLATFORM_NS_HYPERVISOR_PRESENT=0
 make
 
 # Build Hafnium
-export PATH=${WORKSPACE}/prebuilts/linux-x64/clang/bin:${WORKSPACE}/prebuilts/linux-x64/dtc:$PATH
+export PATH=${WORKSPACE}/hafnium/prebuilts/linux-x64/clang/bin:${WORKSPACE}/hafnium/prebuilts/linux-x64/dtc:$PATH
 cd ${WORKSPACE}/hafnium
 echo "Building Hafnium."
 make PROJECT=reference
@@ -25,7 +25,7 @@ make PROJECT=reference
 # Build TF-A
 cd ${WORKSPACE}/trusted-firmware-a
 echo "Building TF-A."
-make PLAT=fvp DEBUG=1 \
+make CROSS_COMPILE=aarch64-none-elf- PLAT=fvp DEBUG=1 \
 	BL33=${WORKSPACE}/ff-a-acs/build/output/vm1.bin \
 	BL32=${WORKSPACE}/hafnium/out/reference/secure_aem_v8a_fvp_clang/hafnium.bin \
 	SP_LAYOUT_FILE=${WORKSPACE}/ff-a-acs/platform/manifest/tgt_tfa_fvp/sp_layout.json \
