5c2f6ba036c673accfd7ad76ec487a172e8a6117
Olivier Deprez <olivier.deprez@arm.com>
2023-11-16 11:18:21 +0100

    acs: add eSPI support to FVP model invocation
    Hafnium requires the model to implement GIC extended SPI support.
    Add to the FVP model command line invocation.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: I76469f9df0e7a08b82c6b28e575b6e171ecd2bb7

--- acs/runner.sh
+++ acs/runner.sh
@@ -31,6 +31,11 @@ ${WORKSPACE}/../fvp/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3/FVP_Base_RevC-2xA
 -C bp.dram_metadata.is_enabled=1 \
 -C cluster0.memory_tagging_support_level=2 \
 -C cluster1.memory_tagging_support_level=2 \
+-C gic_distributor.ARE-fixed-to-one=1 \
+-C cluster0.gicv3.extended-interrupt-range-support=1 \
+-C cluster1.gicv3.extended-interrupt-range-support=1 \
+-C gic_distributor.extended-ppi-count=64 \
+-C gic_distributor.extended-spi-count=1024 \
 -C bp.pl011_uart0.out_file=${WORKSPACE}/logs/fvp-uart0.log \
 -C bp.pl011_uart1.out_file=${WORKSPACE}/logs/fvp-uart1.log \
 -C bp.pl011_uart2.out_file=${WORKSPACE}/logs/fvp-uart2.log \
