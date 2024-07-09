aaece90b26aa3e613d41d9c7a299ca9ecf3f11cb
J-Alves <joao.alves@arm.com>
2024-07-09 09:55:28 +0100

    acs: update the builder according to new release
    Signed-off-by: J-Alves <joao.alves@arm.com>
    Change-Id: Ia61cf22f3eebc32da94ef57f12d8734dc6e7bf4f

--- acs/builder.sh
+++ acs/builder.sh
@@ -10,10 +10,16 @@ cd ${WORKSPACE}/ff-a-acs
 mkdir build
 cd build
 echo "Building ACS test suite."
+
 cmake ../ -G"Unix Makefiles" \
 	-DCROSS_COMPILE=aarch64-none-elf- \
 	-DTARGET=tgt_tfa_fvp \
-	-DPLATFORM_NS_HYPERVISOR_PRESENT=0
+	-DPLATFORM_FFA_V_ALL=1 \
+	-DPLATFORM_NS_HYPERVISOR_PRESENT=0 \
+	-DPLATFORM_SP_EL=1 \
+	-DENABLE_BTI=ON \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DSUITE=all
 make
 
 # Build Hafnium
@@ -31,12 +37,15 @@ make CROSS_COMPILE=aarch64-none-elf- PLAT=fvp DEBUG=1 \
 	BL32=${WORKSPACE}/hafnium/out/reference/secure_aem_v8a_fvp_vhe_clang/hafnium.bin \
 	SP_LAYOUT_FILE=${WORKSPACE}/ff-a-acs/platform/manifest/tgt_tfa_fvp/sp_layout.json \
 	ARM_SPMC_MANIFEST_DTS=${WORKSPACE}/ff-a-acs/platform/manifest/tgt_tfa_fvp/fvp_spmc_manifest.dts \
-	ARM_BL2_SP_LIST_DTS=${WORKSPACE}/ff-a-acs/platform/manifest/tgt_tfa_fvp/fvp_tb_fw_config.dts \
-	ARM_ARCH_MINOR=5 BRANCH_PROTECTION=1 \
-	CTX_INCLUDE_PAUTH_REGS=1 CTX_INCLUDE_EL2_REGS=1 CTX_INCLUDE_MTE_REGS=1 \
-	GIC_EXT_INTID=1 PLAT_TEST_SPM=1 \
+	ARM_ARCH_MAJOR=8 \
+	ARM_ARCH_MINOR=5 \
+	BRANCH_PROTECTION=1 \
+	GIC_EXT_INTID=1 \
+	PLAT_TEST_SPM=1 \
+	ENABLE_FEAT_MTE2=1 \
 	SPD=spmd \
-	all fip
+	ARM_BL2_SP_LIST_DTS=${WORKSPACE}/trusted-firmware-a/build/fvp/debug/sp_list_fragment.dts \
+	all fip -j8
 
 echo "Finished building all targets."
 cd ${WORKSPACE}
